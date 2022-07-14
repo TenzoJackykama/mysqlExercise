aa07b8ba2547a03257f1bedd90e0ab733b638145
TenzoJackykama <andreabulla71@gmail.com>
2022-07-14 16:53:43 +0200

    first commit

---
+++ src/main/kotlin/dbop.kts
@@ -0,0 +1,43 @@
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
+
+object DbUtils{
+
+    private lateinit var con: Connection
+    private lateinit var url: String
+    private lateinit var user: String
+    private lateinit var password: String
+
+    fun getParameters(url: String, user: String, password: String){
+        this.url = url
+        this.user = user
+        this.password = password
+    }
+
+    fun connectToJdbcDatabase(): Connection {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver")
+            con = DriverManager.getConnection(url, user, password)
+        }catch (e: SQLException){
+            println("connectToJdbcDatabase message: ${e.message}")
+        }
+
+        return con
+    }
+}
+
+fun main() {
+    println("Hello World!")
+    DbUtils.getParameters("jdbc:mysql://localhost:3306/newdb", "root", "eciw#vIniSp")
+
+    try {
+        val connection = DbUtils.connectToJdbcDatabase()
+        println("connection to database: ${connection.catalog}")
+    }catch (e: SQLException){
+        println("connection to database: ${e.message}")
+    }
+}
+
+main()
